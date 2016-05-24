f3a66f6d02bbabcc5fe5facd93743581d10b5a28
Pablo Palácios <ppalacios992@gmail.com>
2016-05-24 10:36:57 -0300

    main: creates EFUParser to handle efu arguments
    This is the main handler to deal with efu commands and arguments.
    
    Change-Id: Idf1bf58e691937429d33f3320a37a145dc9c808c
    Signed-off-by: Pablo Palácios <ppalacios992@gmail.com>

--- efu/__main__.py
+++ efu/__main__.py
@@ -1,8 +1,50 @@
 # Copyright (C) 2016 O.S. Systems Software LTDA.
 # This software is released under the MIT License
 
+import argparse
+
+
+class EFUParser(object):
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.subparsers = self.parser.add_subparsers()
+        self.set_upload_parser()
+        self.args = self.parser.parse_args()
+        self.check_arguments()
+        self.run()
+
+    def set_upload_parser(self):
+        """
+        Creates and configures the upload parser
+        """
+        self.upload_parser = self.subparsers.add_parser(
+            'upload',
+            help='Uploads a patch',
+        )
+        self.upload_parser.add_argument('filename', help='patch file name')
+        self.upload_parser.set_defaults(handler=self.upload_handler)
+
+    def upload_handler(self):
+        """
+        This method will be responsible for calling the upload utility
+        """
+        print('uploading {}'.format(self.args.filename))
+
+    def check_arguments(self):
+        """
+        Checks if args is not empty. This is necessary since we are
+        going to use subparser instead of common arguments.
+        """
+        if not any((self.args._get_args(), self.args._get_kwargs())):
+            self.parser.error('You must provide a command (e.g. upload)')
+
+    def run(self):
+        self.args.handler()
+
 
 def main():
+    EFUParser()
     return 0
 
 
--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -15,3 +15,25 @@ class CLITestCase(unittest.TestCase):
     def test_efu_can_be_called_as_a_module(self):
         response = subprocess.call(['python', '-m', 'efu', '-h'])
         self.assertEqual(response, 0)
+
+    def test_efu_is_parsing_arguments(self):
+        response = subprocess.check_output(['efu', '-h'])
+        self.assertIn('help', response.decode())
+
+    def test_efu_raises_error_when_called_without_arguments(self):
+        with self.assertRaises(subprocess.CalledProcessError):
+            subprocess.check_call('efu')
+
+
+class UploadCommandTestCase(unittest.TestCase):
+
+    def test_upload_command_exists(self):
+        response = subprocess.check_output(['efu', 'upload', '-h'])
+        self.assertIn('upload', response.decode())
+
+    def test_upload_command_requires_a_filename(self):
+        response = subprocess.check_call(['efu', 'upload', 'patch.bin'])
+        self.assertEqual(response, 0)
+
+        with self.assertRaises(subprocess.CalledProcessError):
+            subprocess.check_call(['efu', 'upload'])
