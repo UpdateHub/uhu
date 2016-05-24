6eb194782d5404e1256e19e740103908d622c2b3
Pablo Palácios <ppalacios992@gmail.com>
2016-05-24 10:33:28 -0300

    tests: refactored CLI test methods
    CLITestCase.test_efu_is_a_cli_command now uses find_executable to check
    if efu is in PATH. Also, CLITestCase.test_efu_can_be_called_as_a_module
    is now calling 'efu -h' instead of just 'efu'.
    
    This is necessary since in future, calling efu without arguments will
    raise some kind of error (e.g. 'You must provide a command').
    
    Change-Id: I4294309b4c6b9f75e80ff82c0efd374df2b95e03
    Signed-off-by: Pablo Palácios <ppalacios992@gmail.com>

--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -3,14 +3,15 @@
 
 import subprocess
 import unittest
+from distutils.spawn import find_executable
 
 
 class CLITestCase(unittest.TestCase):
 
     def test_efu_is_a_cli_command(self):
-        response = subprocess.call('efu')
-        self.assertEqual(response, 0)
+        response = find_executable('efu')
+        self.assertTrue(response)
 
     def test_efu_can_be_called_as_a_module(self):
-        response = subprocess.call(['python', '-m', 'efu'])
+        response = subprocess.call(['python', '-m', 'efu', '-h'])
         self.assertEqual(response, 0)
