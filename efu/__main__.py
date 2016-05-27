7decf67e37f59a0b911bf843698013f10cd540b7
Pablo Palácios <ppalacios992@gmail.com>
2016-05-27 09:45:04 -0300

    CLIParser: moved EFUParser to CLIParser
    Renamed EFUParser to CLIParser and moved it to its own module.
    
    Change-Id: I853d0b50b3fb2608aa2d1820ef24e732ebca8e87
    Signed-off-by: Pablo Palácios <ppalacios992@gmail.com>

--- efu/__main__.py
+++ efu/__main__.py
@@ -1,58 +1,13 @@
 # Copyright (C) 2016 O.S. Systems Software LTDA.
 # This software is released under the MIT License
 
-import argparse
 import sys
 
-from efu.upload import upload_patch
-
-
-class EFUParser(object):
-
-    def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.subparsers = self.parser.add_subparsers()
-        self.set_upload_parser()
-        self.args = self.parser.parse_args()
-        self.check_has_arguments()
-        self.run()
-
-    def set_upload_parser(self):
-        """
-        Creates and configures the upload parser
-        """
-        self.upload_parser = self.subparsers.add_parser(
-            'upload',
-            help='Uploads a patch',
-        )
-        self.upload_parser.add_argument('filename', help='patch file name')
-        self.upload_parser.set_defaults(handler=self.upload_handler)
-
-    def upload_handler(self):
-        """
-        This method will be responsible for validating arguments and
-        sending them to the upload utility
-        """
-        filename = self.args.filename
-        try:
-            upload_patch(filename)
-        except FileNotFoundError:
-            self.parser.error('file {} does not exist'.format(filename))
-
-    def check_has_arguments(self):
-        """
-        Checks if args is not empty. This is necessary since we are
-        going to use subparser instead of common arguments.
-        """
-        if len(sys.argv) == 1:
-            self.parser.error('You must provide a command (e.g. upload)')
-
-    def run(self):
-        self.args.handler()
+from .cliparser import CLIParser
 
 
 def main():
-    EFUParser()
+    CLIParser()
     return 0
 
 
