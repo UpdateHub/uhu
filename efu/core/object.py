1d7051c8871da70dba18da94911f58de4b37a503
Pablo Palácios <ppalacios992@gmail.com>
2016-09-21 16:43:01 -0300

    Object: added __str__ method
    Change-Id: I54b50895b81da6985e9f7a188d9ae93f9c9988cb
    Signed-off-by: Pablo Palácios <ppalacios992@gmail.com>

--- efu/core/object.py
+++ efu/core/object.py
@@ -2,11 +2,14 @@
 # This software is released under the MIT License
 
 import hashlib
+import itertools
 import os
-from itertools import count
 
 from ..metadata import ObjectMetadata
-from ..utils import get_chunk_size
+from ..utils import get_chunk_size, yes_or_no
+
+
+DEVICE_OPTIONS = ['truncate', 'seek', 'filesystem']
 
 
 class Chunk:
@@ -39,7 +42,7 @@ class Object:
         self.loaded = False
 
         self._fd = None
-        self._chunk_number = count()
+        self._chunk_number = itertools.count()
 
     def load(self):
         if self.loaded:
@@ -73,3 +76,60 @@ class Object:
 
     def __len__(self):
         return len(self.chunks)
+
+    def __str__(self):
+        s = []
+        s.append('{} [mode: {}]'.format(self.filename, self.metadata.mode))
+        s.append('')
+        # compressed option
+        compressed = self.options.get('compressed')
+        if compressed is not None:
+            line = '      Compressed file:   {}'.format(yes_or_no(compressed))
+            if compressed:
+                line += ' [uncompressed size: {}B]'.format(
+                    self.options.get('required-uncompressed-size'))
+            s.append(line)
+        # device option
+        device = self.options.get('target-device')
+        if device is not None:
+            line = '      Target device:     {}'.format(device)
+            device_options = {option: self.options.get(option)
+                              for option in DEVICE_OPTIONS}
+            if any(device_options.values()):
+                truncate = device_options['truncate']
+                if truncate is not None:
+                    device_options['truncate'] = yes_or_no(truncate)
+                device_options = ['{}: {}'.format(k, device_options[k])
+                                  for k in sorted(device_options)
+                                  if device_options[k] is not None]
+                line += ' [{}]'.format(', '.join(device_options))
+            s.append(line)
+        # format option
+        format_ = self.options.get('format?')
+        if format_ is not None:
+            line = '      Format device:     {}'.format(yes_or_no(format_))
+            format_options = self.options.get('format-options')
+            if format_options:
+                line += '[options: "{}"]'.format(format_options)
+            s.append(line)
+        # mount options
+        mount = self.options.get('mount-options')
+        if mount is not None:
+            s.append('      Mount options:     "{}"'.format(mount))
+        # target path option
+        path = self.options.get('target-path')
+        if path is not None:
+            s.append('      Target path:       {}'.format(path))
+        # chunk size option
+        chunk = self.options.get('chunk-size')
+        if chunk is not None:
+            s.append('      Chunk size:        {}'.format(chunk))
+        # skip option
+        skip = self.options.get('skip')
+        if skip is not None:
+            s.append('      Skip from source:  {}'.format(skip))
+        # count option
+        count = self.options.get('count')
+        if count is not None:
+            s.append('      Count:             {}'.format(count))
+        return '\n'.join(s)
--- efu/core/package.py
+++ efu/core/package.py
@@ -6,12 +6,9 @@ from collections import OrderedDict
 
 from ..http.request import Request
 from ..metadata import ObjectMetadata, PackageMetadata
-from ..utils import get_server_url, yes_or_no
+from ..utils import get_server_url
 
 from . import Object
-
-
-DEVICE_OPTIONS = ['truncate', 'seek', 'filesystem']
 
 
 class Package:
@@ -110,62 +107,6 @@ class Package:
         else:
             s.append('Objects: None')
         for obj_id in sorted(self.objects):
-            obj = self.objects[obj_id]
             s.append('')
-            s.append('  {}# {} [mode: {}]'.format(
-                obj_id, obj.metadata.filename, obj.metadata.mode))
-            s.append('')
-            # compressed option
-            compressed = obj.options.get('compressed')
-            if compressed is not None:
-                line = '      Compressed file:   {}'.format(
-                    yes_or_no(compressed))
-                if compressed:
-                    line += ' [uncompressed size: {}B]'.format(
-                        obj.options.get('required-uncompressed-size'))
-                s.append(line)
-            # device option
-            device = obj.options.get('target-device')
-            if device is not None:
-                line = '      Target device:     {}'.format(device)
-                device_options = {option: obj.options.get(option)
-                                  for option in DEVICE_OPTIONS}
-                if any(device_options.values()):
-                    truncate = device_options['truncate']
-                    if truncate is not None:
-                        device_options['truncate'] = yes_or_no(truncate)
-                    device_options = ['{}: {}'.format(k, device_options[k])
-                                      for k in sorted(device_options)
-                                      if device_options[k] is not None]
-                    line += ' [{}]'.format(', '.join(device_options))
-                s.append(line)
-            # format option
-            format_ = obj.options.get('format?')
-            if format_ is not None:
-                line = '      Format device:     {}'.format(
-                    yes_or_no(format_))
-                format_options = obj.options.get('format-options')
-                if format_options:
-                    line += '[options: "{}"]'.format(format_options)
-                s.append(line)
-            # mount options
-            mount = obj.options.get('mount-options')
-            if mount is not None:
-                s.append('      Mount options:     "{}"'.format(mount))
-            # target path option
-            path = obj.options.get('target-path')
-            if path is not None:
-                s.append('      Target path:       {}'.format(path))
-            # chunk size option
-            chunk = obj.options.get('chunk-size')
-            if chunk is not None:
-                s.append('      Chunk size:        {}'.format(chunk))
-            # skip option
-            skip = obj.options.get('skip')
-            if skip is not None:
-                s.append('      Skip from source:  {}'.format(skip))
-            # count option
-            count = obj.options.get('count')
-            if count is not None:
-                s.append('      Count:             {}'.format(count))
+            s.append('  {}# {}'.format(obj_id, str(self.objects[obj_id])))
         return '\n'.join(s)
