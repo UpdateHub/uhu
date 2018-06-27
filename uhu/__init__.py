b40ab12d84ccfe858d76686bec75a1465c6844cc
Otavio Salvador <otavio@ossystems.com.br>
2018-06-27 14:46:35 -0300

    Bump to 1.0.3
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- uhu/__init__.py
+++ uhu/__init__.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2017 O.S. Systems Software LTDA.
+# Copyright (C) 2017, 2018 O.S. Systems Software LTDA.
 # SPDX-License-Identifier: GPL-2.0
 
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 
 def get_version():
